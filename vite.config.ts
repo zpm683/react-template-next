1ed1ec6eedc08fa4f84b7f1e99e6ee407e14a7a7
下午茶溢栈 <zpm683@qq.com>
2023-03-08 09:39:27 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,52 +1,67 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { visualizer } from "rollup-plugin-visualizer";
 import progress from "vite-plugin-progress";
 import eslint from "vite-plugin-eslint";
 import colors from "picocolors";
+import { viteSingleFile } from "vite-plugin-singlefile";
+
+const ENV_PREFIX = "ENV_";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths(),
-    eslint({ cache: true }),
-    {
-      ...visualizer(),
-      apply: "build",
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), ENV_PREFIX);
+
+  const buildInSingleFile =
+    command === "build" && Boolean(env.ENV_BUILD_IN_SINGLEFILE);
+
+  buildInSingleFile && console.log("build in single file!");
+
+  return {
+    plugins: [
+      react(),
+      tsconfigPaths(),
+      eslint({ cache: true }),
+      buildInSingleFile && viteSingleFile({ removeViteModuleLoader: true }),
+      {
+        ...visualizer(),
+        apply: "build",
+      },
+      progress({
+        format: `${colors.green(colors.bold("Building"))} ${colors.cyan(
+          "[:bar]",
+        )} :percent`,
+      }),
+    ],
+    envPrefix: ENV_PREFIX,
+    server: {
+      port: 3000,
+      host: true,
+      cors: true,
     },
-    progress({
-      format: `${colors.green(colors.bold("Building"))} ${colors.cyan(
-        "[:bar]",
-      )} :percent`,
-    }),
-  ],
-  envPrefix: "ENV_",
-  server: {
-    port: 3000,
-    host: true,
-    cors: true,
-  },
-  test: {
-    globals: true,
-    environment: "happy-dom",
-    setupFiles: "./src/test/setup.ts",
-  },
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ["react", "react-dom", "react-router-dom"],
-        },
-        chunkFileNames: "assets/js/[name]-[hash].js",
-        entryFileNames: "assets/js/[name]-[hash].js",
-        assetFileNames: "assets/[ext]/[name]-[hash].[ext]",
+    test: {
+      globals: true,
+      environment: "happy-dom",
+      setupFiles: "./src/test/setup.ts",
+    },
+    build: {
+      outDir: "dist",
+      rollupOptions: {
+        output: buildInSingleFile
+          ? {}
+          : {
+              manualChunks: {
+                react: ["react", "react-dom", "react-router-dom"],
+              },
+              chunkFileNames: "assets/js/[name]-[hash].js",
+              entryFileNames: "assets/js/[name]-[hash].js",
+              assetFileNames: "assets/[ext]/[name]-[hash].[ext]",
+            },
       },
     },
-  },
+  };
 });
