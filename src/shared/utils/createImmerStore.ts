e922027188b8540dedfe2321d5714264e6db87df
下午茶溢栈 <zpm683@qq.com>
2023-03-08 10:38:00 +0800

    Create createImmerStore.ts

---
+++ src/shared/utils/createImmerStore.ts
@@ -0,0 +1,13 @@
+import { create, StateCreator } from "zustand";
+import { immer } from "zustand/middleware/immer";
+
+type Initializer<T extends Object> = StateCreator<
+  T,
+  [["zustand/immer", never]],
+  [],
+  T
+>;
+
+export const createImmerStore = <T extends Object>(
+  initializer: Initializer<T>,
+) => create(immer<T>(initializer));
