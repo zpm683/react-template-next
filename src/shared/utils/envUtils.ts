2cd51392306322b5c553fd0a6ae1fec239008277
下午茶溢栈 <zpm683@qq.com>
2023-04-20 10:27:11 +0800

    Update envUtils.ts

--- src/shared/utils/envUtils.ts
+++ src/shared/utils/envUtils.ts
@@ -1,6 +1,21 @@
-export const isBrowser = () =>
+const isBrowser = () =>
   !!(
     typeof window !== "undefined" &&
     window.document &&
     window.document.createElement
   );
+
+const isSafari = () =>
+  /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
+const getBaseURL = () => import.meta.env.BASE_URL;
+
+const getApiServerURL = () => import.meta.env.ENV_API_URL;
+
+const getApiTimeOut = () => {
+  const timeout = Number(import.meta.env.ENV_API_TIME_OUT);
+
+  return Number.isNaN(timeout) ? undefined : timeout;
+};
+
+export { isSafari, isBrowser, getBaseURL, getApiServerURL, getApiTimeOut };
