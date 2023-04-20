cc98859ee303be43e79b09fbf00809ec81e29bed
下午茶溢栈 <zpm683@qq.com>
2023-04-20 09:00:47 +0800

    Load.tsx

---
+++ src/shared/components/Load.tsx
@@ -0,0 +1,9 @@
+import { ReactNode, Suspense } from "react";
+
+const Loading = () => <div>Loading...</div>
+
+const lazyLoad = (children: ReactNode): ReactNode => (
+  <Suspense fallback={<Loading />}>{children}</Suspense>
+);
+
+export { lazyLoad };
