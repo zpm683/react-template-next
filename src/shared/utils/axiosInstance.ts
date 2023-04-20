a903a1b9ad202f2da6b4f6657d9f27f074fd8ce7
下午茶溢栈 <zpm683@qq.com>
2023-04-20 10:28:02 +0800

    Create axiosInstance.ts

---
+++ src/shared/utils/axiosInstance.ts
@@ -0,0 +1,145 @@
+import axios from "axios";
+import qs from "qs";
+import { produce } from "immer";
+
+import type {
+  AxiosInstance as RawAxiosInstance,
+  AxiosRequestConfig,
+} from "axios";
+import type { IStringifyOptions } from "qs";
+
+import { getApiServerURL, getApiTimeOut } from "./envUtils";
+
+type Result<T> = {
+  success: boolean;
+  code: number;
+  msg: string;
+  data: T;
+};
+
+type RequestConfig = AxiosRequestConfig & {
+  /**
+   * @param indices
+   * qs.stringify({ a: ['b', 'c'] }, { arrayFormat: 'indices' })
+   * 'a[0]=b&a[1]=c'
+   * @param brackets
+   * qs.stringify({ a: ['b', 'c'] }, { arrayFormat: 'brackets' })
+   * 'a[]=b&a[]=c'
+   * @param repeat
+   * qs.stringify({ a: ['b', 'c'] }, { arrayFormat: 'repeat' })
+   * 'a=b&a=c'
+   * @param comma
+   * qs.stringify({ a: ['b', 'c'] }, { arrayFormat: 'comma' })
+   * 'a=b,c'
+   */
+  arrayFormat?: IStringifyOptions["arrayFormat"];
+};
+
+type AxiosInstance = Omit<
+  RawAxiosInstance,
+  "get" | "post" | "put" | "delete"
+> & {
+  get: <T = unknown, R = Result<T>>(
+    url: string,
+    params?: Record<string, unknown>,
+    config?: RequestConfig,
+  ) => Promise<R>;
+
+  post: <T = unknown, R = Result<T>>(
+    url: string,
+    data?: unknown,
+    config?: AxiosRequestConfig,
+  ) => Promise<R>;
+
+  put: <T = unknown, R = Result<T>>(
+    url: string,
+    data?: unknown,
+    config?: AxiosRequestConfig,
+  ) => Promise<R>;
+
+  delete: <T = unknown, R = Result<T>>(
+    url: string,
+    params?: Record<string, unknown>,
+    config?: RequestConfig,
+  ) => Promise<R>;
+};
+
+/**
+ * request interceptor
+ */
+const interceptRequest = (instance: RawAxiosInstance) => {
+  instance.interceptors.request.use((config) => {
+    return config;
+  });
+};
+
+/**
+ * response interceptor
+ */
+const interceptResponse = (instance: RawAxiosInstance) => {
+  instance.interceptors.response.use((res) => {
+    return res.data;
+  });
+};
+
+/**
+ *  convert request
+ */
+const setRequestProxy = (instance: RawAxiosInstance) => {
+  const _get = instance.get;
+  const _delete = instance.delete;
+
+  const appendParamsToUrl = (
+    url: string,
+    params?: Record<string, unknown>,
+    arrayFormat?: RequestConfig["arrayFormat"],
+  ) => {
+    return `${url}${qs.stringify(params, {
+      addQueryPrefix: true,
+      arrayFormat: arrayFormat ?? "repeat",
+    })}`;
+  };
+
+  const filterConfig = (config: RequestConfig) => {
+    return produce(config, (draft) => {
+      delete draft.arrayFormat;
+    });
+  };
+
+  instance.get = (
+    url: string,
+    params?: Record<string, unknown>,
+    config?: RequestConfig,
+  ) => {
+    return _get(
+      appendParamsToUrl(url, params, config?.arrayFormat),
+      config?.arrayFormat ? filterConfig(config) : config,
+    );
+  };
+
+  instance.delete = (
+    url: string,
+    params?: Record<string, unknown>,
+    config?: RequestConfig,
+  ) => {
+    return _delete(
+      appendParamsToUrl(url, params, config?.arrayFormat),
+      config?.arrayFormat ? filterConfig(config) : config,
+    );
+  };
+};
+
+const createInstance = () => {
+  const axiosInstance = axios.create({
+    baseURL: getApiServerURL(),
+    timeout: getApiTimeOut(),
+  });
+
+  interceptRequest(axiosInstance);
+  interceptResponse(axiosInstance);
+  setRequestProxy(axiosInstance);
+
+  return axiosInstance as AxiosInstance;
+};
+
+export const axiosInstance = createInstance();
